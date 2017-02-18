e2e5e9b09e0875e0ec89ddbe73afe0588321eb7a
Robert Northard <robertnorthard@googlemail.com>
2017-02-18 22:37:57 +0000

    Initialised repository with gist client.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Github Gist
+
+A JavaScript library for creating anonymous gists.
+
+# Usage
+
+1. First add jQuery to your project's dependencies.
+
+2. Include `gist.min.js` in your project.
+
+`bower install gist --save`
+
+# Example
+
+      var gistName = "HelloWorld"
+      var gistDesciption = "An example gist."
+      var payload = {
+          data: "Hello, World!"
+      };
+
+      gist.create(
+          gistName,
+          gistDesciption,
+          JSON.stringify(payload),
+          function(success) {
+              // handle success
+              console.log(gist.getGistUrl(success));
+          },
+          function(error) {
+              // handle error 
+              console.log(e);
+          });
+
+See [demo](https://github.com/RobertNorthard/bower-gist/test.html) for a demo of this code.
+
+# Contribute
+
+Please feel free to contribute. Pull requests are welcome.
+
---
+++ gist.js
@@ -0,0 +1,42 @@
+/*
+ *  author: Robert Northard
+ *  version: 1.0.0
+ */
+'use strict'
+
+var gist = (function($) {
+
+    var client = {};
+
+    client.create = function(name, description, data, success, error) {
+
+        var payload = {
+            "description": description,
+            "files": {
+                "file1.txt": {
+                    "content": data
+                }
+            }
+        };
+
+        $.ajax({
+                url: 'https://api.github.com/gists',
+                type: 'POST',
+                dataType: 'json',
+                data: JSON.stringify(payload)
+            })
+            .success(function(result) {
+                success(result)
+            })
+            .error(function(result) {
+                error(result)
+            })
+    }
+
+    client.getGistUrl = function(result) {
+        return 'https://gist.github.com/anonymous/' + result.id + '/raw';
+    }
+
+    return client;
+
+})(jQuery);
